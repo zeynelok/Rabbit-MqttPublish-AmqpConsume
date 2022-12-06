bd812abaa7ef94b140638e3e022da38247af8a8d
Zeynel OK <zeynel_2406@hotmail.com>
2022-12-06 11:18:57 +0300

    Initial commit

---
+++ MqttPublisher/MqttPublisher/Program.cs
@@ -0,0 +1,44 @@
+﻿// See https://aka.ms/new-console-template for more information
+using MQTTnet;
+using MQTTnet.Client;
+
+
+await Publish_Application_Message();
+
+Console.ReadLine();
+
+
+static async Task Publish_Application_Message()
+{
+
+    var mqttFactory = new MqttFactory();
+
+    using (var mqttClient = mqttFactory.CreateMqttClient())
+    {
+        var mqttClientOptions = new MqttClientOptionsBuilder()
+            //.WithWillQualityOfServiceLevel(MQTTnet.Protocol.MqttQualityOfServiceLevel.AtLeastOnce).WithCleanSession(false)
+            .WithTcpServer("127.0.0.1",1883)
+            .Build();
+
+        await mqttClient.ConnectAsync(mqttClientOptions, CancellationToken.None);
+
+        var value = 0;
+        while (true)
+        {
+            var applicationMessage = new MqttApplicationMessageBuilder()
+           .WithTopic("testkey")
+           .WithPayload($"{value}")
+           .Build();
+            await mqttClient.PublishAsync(applicationMessage, CancellationToken.None);
+            Console.WriteLine("MQTT application message is published. Value: " + value);
+            value++;
+            Thread.Sleep(5000);
+
+        }
+
+
+        await mqttClient.DisconnectAsync();
+
+        Console.WriteLine("MQTT application message is published.");
+    }
+}
---
+++ RabbitConsumer/RabbitConsumer/Program.cs
@@ -0,0 +1,62 @@
+﻿// See https://aka.ms/new-console-template for more information
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+Console.WriteLine("Hello, World!");
+
+var subs = new Subscriber();
+subs.Subscribe();
+
+Console.ReadLine();
+
+internal class Subscriber
+{
+    private readonly IModel _channel;
+    public Subscriber()
+    {
+        var connectionFactory = new ConnectionFactory();
+        IList<AmqpTcpEndpoint> hosts = new List<AmqpTcpEndpoint>()
+        {
+            new AmqpTcpEndpoint{HostName="127.0.0.1",Port=5671},
+            //new AmqpTcpEndpoint{HostName="127.0.0.1",Port=5672},
+            //new AmqpTcpEndpoint{HostName="127.0.0.1",Port=5673},
+
+        };
+
+        var connection = connectionFactory.CreateConnection(hosts);
+        Console.WriteLine("Connection Created");
+
+        _channel = connection.CreateModel();
+
+    }
+
+    public void Subscribe()
+    {
+        var queueName = $"queue.rabbit-test";
+        _channel.QueueDeclare(queueName, true, false, false);
+        _channel.QueueBind(queueName, "amq.topic", "testkey");
+
+        _channel.BasicQos(prefetchSize: 0, prefetchCount: 1, global: false);
+
+        var consumer = new EventingBasicConsumer(_channel);
+
+        consumer.Received += MessageReceived;
+
+        _channel.BasicConsume(queueName, false, consumer);
+        Console.WriteLine("Waiting for message...");
+    }
+
+    void MessageReceived(object? sender, BasicDeliverEventArgs ea)
+    {
+
+        var body = ea.Body.ToArray();
+        var message = Encoding.UTF8.GetString(body);
+        Console.WriteLine($"Recieved Message : {message}");
+        _channel.BasicAck(ea.DeliveryTag, false);
+
+        Thread.Sleep(1000);
+
+
+    }
+}
